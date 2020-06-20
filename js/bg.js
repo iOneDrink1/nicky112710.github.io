8c2773479c581a2c86b0ef241f97401e88b40717
nicky112710 <67174529+nicky112710@users.noreply.github.com>
2020-06-20 10:44:16 +0900

    Update bg.js

--- js/bg.js
+++ js/bg.js
@@ -1,5 +1,5 @@
 const UNSPLASH_URL = "http://source.unsplash.com/random";
-const body = document.querySelecter("body");
+const body = document.querySelector("body");
 function paintBg(){
   body.style.backgroundImage = `url($UNSPLSH_URL})`;
 }
